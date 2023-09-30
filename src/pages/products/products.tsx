c717ce2cc179f16f70b0d804c6913d1e59615eab
Ed Angelis <ed4ngelis@gmail.com>
2023-09-30 20:48:55 -0300

    products lists created

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,93 +1,107 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import trashIcon from "../../assets/icons/trash-icon.svg";
+import cartIcon from "../../assets/icons/cart-icon.svg";
+import homeIcon from "../../assets/icons/home-icon.svg";
 import IProducts from "../../models/products.model";
-import { getProducts, getBuyer, updateCart } from "../../app.service";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
+import arrowIcon from "../../assets/icons/arrow-icon.png";
+import {
+  getProducts,
+  getBuyer,
+  updateCart,
+  deleteProduct,
+} from "../../app.service";
 import IBuyer from "../../models/buyer.model";
-import { Grid, TextField } from "@mui/material";
 import "./products.css";
 
-export default function Products (){
+export default function Products() {
+  const navigate = useNavigate();
+  const [products, setProducts] = useState<IProducts[]>([]);
+  const [buyer, setBuyer] = useState<IBuyer>();
 
-    const [products, setProducts] = useState<IProducts[]>([]);
-    const [buyer, setBuyer] = useState<IBuyer>();
+  const [filteredProducts, setFilteredProducts] = useState<IProducts[]>([]);
 
-    const [filteredProducts, setFilteredProducts] = useState<IProducts[]>([]);
-    
-    useEffect(() => {
-        loadProducts();
-        loadBuyer();
+  useEffect(() => {
+    loadProducts();
+    loadBuyer();
+  }, []);
 
-      }, []);
+  const loadProducts = async () => {
+    const response = await getProducts();
+    setProducts(response.data);
+    setFilteredProducts(response.data);
+  };
 
-      const loadProducts = async () => {
-        const response = await getProducts();
-        setProducts(response.data);
-        setFilteredProducts(response.data);
-      };
+  const loadBuyer = async () => {
+    const response = await getBuyer();
+    await setBuyer(response);
+  };
 
-      const loadBuyer = async () => {
-        const response = await getBuyer();
-        await setBuyer(response);
-      };
+  const addItemToCart = async (product: IProducts) => {
+    if (buyer?.cart) {
+      buyer.cart.items.push({ product, qt: 1 });
+      try {
+        const resp = await updateCart(buyer);
+        console.log(resp);
+        await setBuyer(resp);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
-      const addItemToCart = async (product: IProducts) => {
-        if (buyer?.cart) {
-          buyer.cart.items.push({ product, qt: 1 });
-          try {
-            const resp = await updateCart(buyer);
-            console.log(resp);
-            await setBuyer(resp);
-          } catch (error) {
-            console.log(error);
-          }
-        }
-      };
+  const deleteProd = async (id: string) => {
+    try {
+      await deleteProduct(id);
+      loadProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      const handleFilter = (event: any) => {
-        const value = event.target.value;
-        const temp = products.filter((product) =>
-          product.name.toLowerCase().includes(value.toLowerCase())
-        );
-        setFilteredProducts(temp);
-      };
-
-    return (
-        <>
-        <Grid item className="products" xs={4}>
-          <Grid container>
-            <Grid container>
-              <h1>Products</h1>
-              <AddCircleIcon />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                id="outlined-basic"
-                label="Outlined"
-                variant="outlined"
-                onChange={handleFilter}
-              />
-            </Grid>
-            <Grid container>
-              {filteredProducts.map((product, index) => (
-                <Grid container className="table" key={index}>
-                  <Grid item xs={10}>
-                    <span> {product.name}</span>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <div
-                      onClick={() => {
-                        addItemToCart(product);
-                      }}
-                    >
-                      <PlayCircleFilledIcon />
-                    </div>
-                  </Grid>
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-        </Grid>
-        </>
+  const handleFilter = (event: any) => {
+    const value = event.target.value;
+    const temp = products.filter((product) =>
+      product.name.toLowerCase().includes(value.toLowerCase())
     );
+    setFilteredProducts(temp);
+  };
+
+  const goTo = (path: string) => {
+    navigate(path);
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="top">
+          <img src={homeIcon} alt="home" />
+          <button onClick={() => goTo("/create-product")}>NEW PRODUCT</button>
+          <img src={arrowIcon} alt="" onClick={() => goTo("/cart")} />
+        </div>
+
+        <div className="filter">
+          <input type="text" onChange={(event) => handleFilter(event)} />
+        </div>
+        {filteredProducts.map((product, index) => (
+          <div className="row" key={index}>
+            <img
+              src={trashIcon}
+              alt="delete"
+              onClick={() => deleteProd(product._id)}
+            />
+            <div>
+              <span className="product">{product.name}</span>
+              <span className="badge">{product.badge}</span>
+              <span className="price">{product.price}</span>
+            </div>
+            <img
+              src={cartIcon}
+              alt="cart"
+              onClick={() => addItemToCart(product)}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
