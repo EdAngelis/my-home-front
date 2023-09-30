addea5cca7722d295cae69104143c5c54a2bcfef
Ed Angelis <ed4ngelis@gmail.com>
2023-09-30 20:48:23 -0300

    routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes } from "react-router-dom";
 
-import  Products  from './pages/products/products';
-import Cart  from './pages/cart/cart';
-import Duties from './pages/duties/duties';
-
+import Products from "./pages/products/products";
+import Cart from "./pages/cart/cart";
+import Duties from "./pages/duties/duties";
+import CreateProduct from "./pages/products/create/create_product";
 export interface RouteObject {
   caseSensitive?: boolean;
   children?: RouteObject[];
@@ -12,22 +12,26 @@ export interface RouteObject {
   path?: string;
 }
 const routes = [
-    {
-        path: '/products',
-        index: true,
-        element: <Products />
-    },
-    {
-        path: '/cart',
-        element: <Cart />
-    },
-    {
-        path: '/duties',
-        element: <Duties />
-    }
+  {
+    path: "/products",
+    index: true,
+    element: <Products />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/duties",
+    element: <Duties />,
+  },
+  {
+    path: "/create-product",
+    element: <CreateProduct />,
+  },
 ];
 
 export default function Routes(): JSX.Element {
-    const routing = useRoutes(routes);
-    return <>{routing}</>;
+  const routing = useRoutes(routes);
+  return <>{routing}</>;
 }
