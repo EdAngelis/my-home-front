a358e8ef785d5cf3ef0c36aaa4f1f56d9901871d
Ed Angelis <ed4ngelis@gmail.com>
2023-10-01 17:40:36 -0300

    icons element

---
+++ src/assets/icons/icons.tsx
@@ -0,0 +1,8 @@
+import dim from "./dim.svg";
+import plus from "./plus.svg";
+import arrowIcon from "./arrow-icon.png";
+import cartIcon from "./cart-icon.svg";
+import homeIcon from "./home-icon.svg";
+import trashIcon from "./trash-icon.svg";
+
+export { dim, plus, arrowIcon, cartIcon, homeIcon, trashIcon };
