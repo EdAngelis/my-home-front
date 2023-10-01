cfdbe64110ed09968898b02d35d97bb7de3cb392
Ed Angelis <ed4ngelis@gmail.com>
2023-10-01 17:40:55 -0300

    menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,15 +1,21 @@
-import { Link, Outlet } from 'react-router-dom';
-import './menu.css';
+import { Link, Outlet } from "react-router-dom";
+import "./menu.css";
 
 export default function Menu() {
-    return (
-        <div>
-            <nav className='nav-bar'>
-                <Link to="products">Products</Link>
-                <Link to="cart">Cart</Link>
-                <Link to="duties">Duties</Link>
-            </nav>
-            <Outlet />
-        </div>
-    )
+  return (
+    <div>
+      <nav className="nav-bar">
+        <Link className="li" to="products">
+          Products
+        </Link>
+        <Link className="li" to="cart">
+          Cart
+        </Link>
+        <Link className="li" to="duties">
+          Duties
+        </Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
