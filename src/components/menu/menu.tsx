47b2dcd3a67a4a53fde3ee8de9f385595ffbe79b
Ed Angelis <ed4ngelis@gmail.com>
2023-10-03 22:01:19 -0300

    menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,16 +1,23 @@
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { AppContext } from "../../context";
 import "./menu.css";
 
 export default function Menu() {
+  const { qtItemCart } = useContext(AppContext);
   return (
     <div>
       <nav className="nav-bar">
+        <Link className="li" to="">
+          Home
+        </Link>
         <Link className="li" to="products">
           Products
         </Link>
-        <Link className="li" to="cart">
-          Cart
-        </Link>
+        <div className="li li-cart">
+          <div className="badge-cart">{qtItemCart}</div>
+          <Link to="cart">Cart</Link>
+        </div>
         <Link className="li" to="duties">
           Duties
         </Link>
