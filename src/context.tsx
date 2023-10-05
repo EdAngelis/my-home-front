825ef8c3d293e84f3f3498322a8a8718a15cf665
Ed Angelis <ed4ngelis@gmail.com>
2023-10-04 22:24:27 -0300

    context

--- src/context.tsx
+++ src/context.tsx
@@ -1,10 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext<any>(null);
 
 export function AppProvider({ children }: any) {
+  const [userId, setUserId] = useState<string>("");
+  const [qtItemCart, setQtItemCart] = useState<number>(0);
   return (
-    <AppContext.Provider value={{ qtItemCart: 11 }}>
+    <AppContext.Provider
+      value={{ qtItemCart, userId, setUserId, setQtItemCart }}
+    >
       {children}
     </AppContext.Provider>
   );
