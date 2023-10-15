0ddd39e492f66ec98401df517c8cc3d0f0387470
Ed Angelis <ed4ngelis@gmail.com>
2023-10-15 14:49:44 -0300

    input button refactored

--- src/components/input-button/input-button.tsx
+++ src/components/input-button/input-button.tsx
@@ -4,8 +4,10 @@ type Props = {
   type?: string;
   label: string;
   value: string;
+  description?: string;
   onClick: () => void;
-  onInputChange: (value: any) => void;
+  onInputChange?: (value: any) => void;
+  onKey?: (e: any) => void;
   placeholder?: string;
 };
 
@@ -14,23 +16,34 @@ export default function InputButton({
   onClick,
   type = "text",
   onInputChange,
+  onKey,
   placeholder,
+  description,
 }: Props) {
   const onChange = (event: any) => {
-    onInputChange(event.target.value);
+    onInputChange && onInputChange(event.target.value);
+  };
+
+  const onKeyDown = (event: any) => {
+    if (event.key !== "Enter") return;
+    onKey && onKey(event.target.value);
   };
 
   return (
     <div className={styles.container}>
-      <input
-        className={styles.input}
-        type={type}
-        onChange={onChange}
-        placeholder={placeholder}
-      />
-      <button className={styles.button} onClick={onClick}>
-        {label}
-      </button>
+      <div className={styles.flexContainer}>
+        <input
+          className={styles.input}
+          type={type}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          placeholder={placeholder}
+        />
+        <button className={styles.button} onClick={onClick}>
+          {label}
+        </button>
+      </div>
+      <span className={styles.description}>{description}</span>
     </div>
   );
 }
